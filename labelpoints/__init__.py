5ee66ca5d1e39742a35f5cdcc2f17f4d3ef48891
ScCcWe <1229659207@qq.com>
2020-06-23 23:36:23 +0800

    🔨更新setup

--- labelpoints/__init__.py
+++ labelpoints/__init__.py
@@ -1,5 +1,3 @@
-# flake8: noqa
-
 import logging
 import sys
 
@@ -8,11 +6,7 @@ from qtpy import QT_VERSION
 
 __appname__ = "labelpoints"
 
-# Semantic Versioning 2.0.0: https://semver.org/
-# 1. MAJOR version when you make incompatible API changes;
-# 2. MINOR version when you add functionality in a backwards-compatible manner;
-# 3. PATCH version when you make backwards-compatible bug fixes.
-__version__ = "4.4.0"
+__version__ = "0.0.1"
 
 QT4 = QT_VERSION[0] == "4"
 QT5 = QT_VERSION[0] == "5"
--- setup.py
+++ setup.py
@@ -1,18 +1,29 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r", encoding='utf-8') as fh:
     long_description = fh.read()
+
+install_requires = [
+    "numpy",
+    "matplotlib",
+    "pillow",
+    "qtpy",
+    "termcolor",
+    "imgviz",
+    "PyYAML",
+    "PyQt5",
+]
 
 setuptools.setup(
     name="labelpoints",  # Replace with your own username
     version="0.0.1",
     author="ScCcEe",
     author_email="scccwe@qq.com",
-    description="a simply tool for points",
+    description="a tool for only points",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://https://github.com/ScCcWe/labelpoints",
-    packages=setuptools.find_packages(),
+    packages=install_requires,
     classifiers=[
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.7",
